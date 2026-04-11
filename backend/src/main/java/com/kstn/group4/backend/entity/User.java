40e2b4afbfd636d6b1b010172e93131106541909
Linh126-minari <hirofumiyoshida2712@gmail.com>
2026-04-11 16:30:18 +0700

    feat:add sign in and sign up logic

---
+++ backend/src/main/java/com/kstn/group4/backend/entity/User.java
@@ -0,0 +1,46 @@
+package com.kstn.group4.backend.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "team_id")
+    private Integer teamId;
+
+    public User() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(String username, String email, String password, String role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ backend/src/main/java/com/kstn/group4/backend/payload/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.kstn.group4.backend.payload;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
